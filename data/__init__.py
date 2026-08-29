9dff876f09e971368ab440d649a67b0893377ee3
Tzlil Levy <tzlilevy@cs.huji.ac.il>
2026-08-29 18:27:26 +0300

    Update schema and init imports for deployment

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,18 @@
-from .schema import SkinProfile, Product, RoutinePlan
+"""
+מבני הנתונים של חבילת ה-data.
+"""
+from .schema import (
+    SkinTypeEnum,
+    TimeOfDayEnum,
+    ProductTypeEnum,
+    SkinProfile,
+    Product,
+    RoutineStep,
+    RoutineOutput,
+    RoutineOutput as RoutinePlan,  # תאימות לשמות שונים במנוע
+    ConflictAnalysis
+)
+from .products import SEED_PRODUCTS
 from .concerns import (
     SKIN_TYPES,
     SKIN_CONCERNS,
@@ -6,16 +20,21 @@ from .concerns import (
     CONCERN_TO_INGREDIENTS,
     get_concern_ingredients
 )
-from .products import SEED_PRODUCTS
 
 __all__ = [
+    "SkinTypeEnum",
+    "TimeOfDayEnum",
+    "ProductTypeEnum",
     "SkinProfile",
     "Product",
+    "RoutineStep",
+    "RoutineOutput",
     "RoutinePlan",
+    "ConflictAnalysis",
+    "SEED_PRODUCTS",
     "SKIN_TYPES",
     "SKIN_CONCERNS",
     "CONCERN_SAFETY_FLAGS",
     "CONCERN_TO_INGREDIENTS",
-    "get_concern_ingredients",
-    "SEED_PRODUCTS"
+    "get_concern_ingredients"
 ]
